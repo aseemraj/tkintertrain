7af9e687729e84fa77d8c3cf097e574b4987428c
aseemraj <aseem.mathswiz@gmail.com>
2014-09-19 00:36:14 +0530

    \m/

--- outerline.py
+++ outerline.py
@@ -9,13 +9,16 @@ class Outerline(object):
         self.occupied = False
         self.status = True
         self.train = None
-        if self.lineNo<6:
-            self.body = w.create_rectangle(100, self.lineNo*20,
-            300, self.lineNo*20+8, fill="#080")
+        if self.lineNo<4:
+            self.body = w.create_rectangle(100, self.lineNo*55,
+            300, self.lineNo*55+8, fill="#080")
         else:
-            self.body = w.create_rectangle(900, (self.lineNo-5)*20,
-            1100, (self.lineNo-5)*20+8, fill="#080")
+            self.body = w.create_rectangle(900, (self.lineNo-3)*55,
+            1100, (self.lineNo-3)*55+8, fill="#080")
 
     def update(self, w):
-        if self.occupied==True:
+        if self.occupied:
             w.itemconfigure(self.body, fill="#800")
+        else:
+            w.itemconfigure(self.body, fill="#080")
+
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 class Train(object):
     """Name of the train along with his trainCode, Arrival Time Departure Time"""
-    def __init__(self, w, code, name, arrival, departure, platform=0, outerline=0, status="notarrived"):
+    def __init__(self, w, code, name, arrival, departure, category, platform=0, outerline=0, status="notarrived"):
         self.name = name
         self.code = code
         self.arrival = arrival
@@ -8,6 +8,7 @@ class Train(object):
         self.platform = platform
         self.outerline = outerline
         self.status = status
+        self.category = category
         self.vel = 0
         self.body = w.create_rectangle(-350, 30, 0, 45, fill="#e33")
         self.x = w.coords(self.body)[0]
